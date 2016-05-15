846f1b450a2339e69b91e7f3783116163f35825d
Brian <brian.houng@gmail.com>
2016-05-15 11:13:30 -0700

    changed template to templateUrl

--- scripts/app.js
+++ scripts/app.js
@@ -12,11 +12,7 @@ var myApp = angular.module('myApp', [])
 	            inputName: '@',
 	            inputModel: '=ngModel',
 	        },
-	        template: '<div class="input-group" ng-repeat="val in inputModel track by $index">' +
-	                      '<input type="text" class="form-control" ng-model="inputModel[$index]" name="{{inputName}}"/>' +
-	                      '<span class="input-group-btn"><button class="btn btn-danger" ng-click="removeField($index)" ng-disabled="inputModel.length == 1"><i class="glyphicon glyphicon-minus"></i></button></span>' +
-	                  '</div>' +
-	                  '<button class="btn btn-primary add-field" ng-click="addField()"><i class="glyphicon glyphicon-plus"></i></button>',
+	        templateUrl: 'templates/inputfield.html',
 	        link: function($scope, elem, attrs) {
 	            if($scope.inputModel.length == 0){
 	                $scope.inputModel.length = 1;
